893cbe8fa51a187cf5a76e5f576ff6b707c3243c
tdk-bwcb <ygf2024@gmail.com>
2024-12-21 13:01:24 +0530

    generics done

--- 03_pure_ts/src/generic.ts
+++ 03_pure_ts/src/generic.ts
@@ -25,3 +25,54 @@ interface bottle{
 }
 
 identityFour<bottle>({brand:"sufd", type:345})
+
+function getSearchProducts<T>(products: T[]): T{
+    return products[3]
+}
+// input : array of T, return : one T
+
+
+const getMoreSearchProducts = <T>(products: T[]): T => {
+    // ......
+    const myIndex = 4
+    return products[myIndex]
+}
+
+
+
+interface Database {
+    connection: string,
+    username: string,
+    password: string
+}
+
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object{
+    return{
+        valOne, 
+        valTwo
+    }
+}
+// anotherFunction(3, "4")
+anotherFunction(344, {connection:"sdf", username: "erd", password: "erdh"})
+
+
+
+
+interface Quiz{
+    name: string,
+    type: string
+}
+interface Course{
+    name: string,
+    author: string,
+    aubject: string
+}
+
+class Sellable<T>{
+    public cart: T[] = []
+
+    addToCart(products: T){
+        this.cart.push(products)
+    }
+}
+
